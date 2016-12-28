7efd81e1f722155bbde3ead9c8b22265632a99e4
allan <epicallan.al@gmail.com>
2016-12-28 12:54:54 +0000

    deploy.sh changes

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,13 @@ echo "finished updating pdf prints repo"
 # remove the current pdf folders
 # replace them  with the one in this repo
 
-#docker_containers=(datahub-staging-app datahub) #array
-docker_containers="datahub-staging-app" #array
+docker_containers="datahub-staging-app datahub" #array
+
 for container in $docker_containers
 do
   docker exec $container rm -rf /src/public/pdf #remove pdf folder content
   docker cp ~/pdf-prints $container:/src/public/pdf/ #copy into pdf folder
   echo "finished updating pdf folder for  $container"
+  docker exec $container rm -rf /src/public/pdf/.git #remove git folder
+  echo "removed pdf git folder in $container"
 done
